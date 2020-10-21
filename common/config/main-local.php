3c912ae3260defb78f96ac421bb6106743564c9e
Bayu Gusti Paraya <bayu.gustip7820@gmail.com>
2020-10-21 14:10:24 +0700

    pretty url

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -16,5 +16,16 @@ return [
             // for the mailer to send real emails.
             'useFileTransport' => true,
         ],
+        /* mempercantik url */
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => array(
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+/<id>:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ),
+        ],
     ],
 ];
