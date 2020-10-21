87881b082300d7c4b93fadbb9fa396614f9910c9
Bayu Gusti Paraya <bayu.gustip7820@gmail.com>
2020-10-22 03:19:15 +0700

    view hasil-pesan

--- frontend/views/site/hasil-pesan.php
+++ frontend/views/site/hasil-pesan.php
@@ -1,12 +1,12 @@
 <?php
 if (Yii::$app->session->hasFlash('success')) {
-  echo '<div class="alert alert-success"></div>';
+  echo '<div class="alert alert-success">';
   echo Yii::$app->session->getFlash('success');
   echo '</div>';
   echo '<br>Nama : '.$model->nama;
   echo '<br>Pesan : '.$model->pesan;
 } else if (Yii::$app->session->hasFlash('error')) {
-  echo '<div class="alert alert-error"></div>';
+  echo '<div class="alert alert-error">';
   echo Yii::$app->session->getFlash('Error');
   echo '</div>';
 }
