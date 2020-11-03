47d122514b606718bbcd931dfd7660e46c779d46
Bayu Gusti Paraya <bayu.gustip7820@gmail.com>
2020-11-03 11:56:03 +0700

    tabel akademik.dosen

--- akademik.dosen.sql
+++ akademik.dosen.sql
@@ -6,3 +6,5 @@ nohp varchar(13) );
 
 INSERT INTO dosen VALUES ('198104152006041002', 'Deddy Prayama', 'Padang', '085376419700');
 INSERT INTO dosen (nip, nama, alamat) VALUES ('198104152006041009', 'Deddy Prayama', 'Padang');
+INSERT INTO dosen VALUES ('1811082018', 'Bayu Gusti Parya', 'Payakumbuh', '081271077073');
+INSERT INTO dosen (nip, nama, alamat) VALUES ('01811082018', 'Bayu Gusti Paraya', 'Payakumbuah');
