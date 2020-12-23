8d73a06444bc27673e68a4afb2397f03085d48d3
Bayu Gusti Paraya <bayu.gustip7820@gmail.com>
2020-12-23 19:32:21 +0700

    dropDownList dengan ArrayHelper

--- frontend/views/prodi/_form.php
+++ frontend/views/prodi/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use frontend\models\Jurusan;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Prodi */
@@ -15,7 +17,8 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'nama_prodi')->textInput(['maxlength' => true]) ?>
 
     <!--<?= $form->field($model, 'jurusan')->textInput() ?>-->
-    <?= $form->field($model, 'jurusan')->dropDownList(['1'=>'Teknologi Informasi','2'=>'Teknik Elektro']) ?>
+    <!--<?= $form->field($model, 'jurusan')->dropDownList(['1'=>'Teknologi Informasi','2'=>'Teknik Elektro']) ?>-->
+    <?= $form->field($model, 'jurusan')->dropDownList( ArrayHelper::map(jurusan::find()->all(),'id','nama_jurusan') ) ?>
 
     <?= $form->field($model, 'telpon')->textInput(['maxlength' => true]) ?>
 
