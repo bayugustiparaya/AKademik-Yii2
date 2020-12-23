249a5e65c300e184bfeff17ed8a573afe215cbe1
Bayu Gusti Paraya <bayu.gustip7820@gmail.com>
2020-12-23 18:55:09 +0700

    prodi model

---
+++ frontend/models/Prodi.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "prodi".
+ *
+ * @property int $id_prodi
+ * @property string $nama_prodi
+ * @property int $jurusan
+ * @property string|null $telpon
+ */
+class Prodi extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'prodi';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nama_prodi', 'jurusan'], 'required'],
+            [['jurusan'], 'integer'],
+            [['nama_prodi'], 'string', 'max' => 80],
+            [['telpon'], 'string', 'max' => 10],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id_prodi' => 'Id Prodi',
+            'nama_prodi' => 'Nama Prodi',
+            'jurusan' => 'Jurusan',
+            'telpon' => 'Telpon',
+        ];
+    }
+}
